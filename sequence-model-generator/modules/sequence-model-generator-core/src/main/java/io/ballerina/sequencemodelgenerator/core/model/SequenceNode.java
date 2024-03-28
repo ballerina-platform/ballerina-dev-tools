5a6883f8752eec3e770d23b4e2b8d88bcfe5e563
Nipuna Fernando <nipunafe@gmail.com>
2024-03-28 11:24:23 +0530

    Document the classes in the sequence model

--- sequence-model-generator/modules/sequence-model-generator-core/src/main/java/io/ballerina/sequencemodelgenerator/core/model/SequenceNode.java
+++ sequence-model-generator/modules/sequence-model-generator-core/src/main/java/io/ballerina/sequencemodelgenerator/core/model/SequenceNode.java
@@ -1,3 +1,21 @@
+/*
+ *  Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com)
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package io.ballerina.sequencemodelgenerator.core.model;
 
 import io.ballerina.compiler.api.SemanticModel;
@@ -9,6 +27,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Represents a sequence node in the sequence diagram.
+ *
+ * @since 2201.9.0
+ */
 public class SequenceNode {
 
     public static final String CONDITION_LABEL = "condition";
@@ -29,10 +52,22 @@ public class SequenceNode {
         this.location = location;
     }
 
+    /**
+     * Represents a branch in the sequence node.
+     *
+     * @param label    Label of the branch
+     * @param children Children of the branch
+     * @since 2201.9.0
+     */
     public record Branch(String label, List<SequenceNode> children) {
 
     }
 
+    /**
+     * Represents the kind of the sequence node.
+     *
+     * @since 2201.9.0
+     */
     public enum NodeKind {
         IF,
         WHILE,
@@ -42,6 +77,11 @@ public class SequenceNode {
         RETURN
     }
 
+    /**
+     * Represents the builder for the {@link SequenceNode}.
+     *
+     * @since 2201.9.0
+     */
     public static class Builder {
 
         protected final SemanticModel semanticModel;
