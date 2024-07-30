d102c9dab0ab872ed950f38c9153540300f37d02
Nipuna Fernando <nipunafe@gmail.com>
2024-07-30 12:36:52 +0530

    Add source generation logic for new connections

--- flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/model/node/NewConnection.java
+++ flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/model/node/NewConnection.java
@@ -18,11 +18,18 @@
 
 package io.ballerina.flowmodelgenerator.core.model.node;
 
+import io.ballerina.compiler.syntax.tree.SyntaxKind;
 import io.ballerina.flowmodelgenerator.core.central.Central;
 import io.ballerina.flowmodelgenerator.core.central.CentralProxy;
 import io.ballerina.flowmodelgenerator.core.model.Codedata;
 import io.ballerina.flowmodelgenerator.core.model.FlowNode;
 import io.ballerina.flowmodelgenerator.core.model.NodeBuilder;
+import io.ballerina.flowmodelgenerator.core.model.Property;
+import io.ballerina.flowmodelgenerator.core.model.SourceBuilder;
+
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.Set;
 
 /**
  * Represents a new connection node in the flow model.
@@ -47,6 +54,69 @@ public class NewConnection extends NodeBuilder {
 
     @Override
     public String toSource(FlowNode flowNode) {
-        return null;
+        SourceBuilder sourceBuilder = new SourceBuilder();
+
+        Optional<Property> variable = flowNode.getProperty(Property.VARIABLE_KEY);
+        Optional<Property> type = flowNode.getProperty(PropertiesBuilder.DATA_TYPE_KEY);
+
+        if (type.isPresent() && variable.isPresent()) {
+            sourceBuilder
+                    .keyword(SyntaxKind.FINAL_KEYWORD)
+                    .expressionWithType(type.get(), variable.get())
+                    .keyword(SyntaxKind.EQUAL_TOKEN)
+                    .keyword(SyntaxKind.CHECK_KEYWORD)
+                    .keyword(SyntaxKind.NEW_KEYWORD)
+                    .keyword(SyntaxKind.OPEN_PAREN_TOKEN);
+        }
+
+        FlowNode nodeTemplate = central.getNodeTemplate(flowNode.codedata().node(), flowNode.codedata().module(),
+                flowNode.codedata().symbol());
+        Set<String> keys = nodeTemplate.properties().keySet();
+        keys.remove("variable");
+        keys.remove("type");
+        keys.remove("scope");
+        Iterator<String> iterator = keys.iterator();
+
+        if (!keys.isEmpty()) {
+            String firstKey = iterator.next();
+            Optional<Property> firstParameter = flowNode.getProperty(firstKey);
+            Optional<Property> firstTemplateParameter = nodeTemplate.getProperty(firstKey);
+            if (firstParameter.isPresent() && firstTemplateParameter.isPresent() &&
+                    !isDefaultValue(firstParameter.get(), firstTemplateParameter.get())) {
+                sourceBuilder.expression(firstParameter.get());
+            }
+
+            boolean hasEmptyParam = false;
+            while (iterator.hasNext()) {
+                String parameterKey = iterator.next();
+                Optional<Property> parameter = flowNode.getProperty(parameterKey);
+                Optional<Property> templateParameter = nodeTemplate.getProperty(parameterKey);
+
+                if (parameter.isEmpty() || templateParameter.isEmpty() || parameter.get().value() == null ||
+                        isDefaultValue(parameter.get(), templateParameter.get())) {
+                    hasEmptyParam = true;
+                    continue;
+                }
+
+                sourceBuilder.keyword(SyntaxKind.COMMA_TOKEN);
+                if (hasEmptyParam) {
+                    sourceBuilder
+                            .name(parameterKey)
+                            .keyword(SyntaxKind.EQUAL_TOKEN);
+                    hasEmptyParam = false;
+                }
+                sourceBuilder.expression(parameter.get());
+            }
+        }
+
+        sourceBuilder
+                .keyword(SyntaxKind.CLOSE_PAREN_TOKEN)
+                .endOfStatement();
+
+        return sourceBuilder.build(false);
+    }
+
+    private boolean isDefaultValue(Property node, Property templateNode) {
+        return node.optional() && node.value().equals(templateNode.value());
     }
 }
