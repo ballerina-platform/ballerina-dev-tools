96c42eebf327b10cb8d44292bccdc4bfaac969f1
Nipuna Fernando <nipunafe@gmail.com>
2024-07-31 16:50:28 +0530

    Implement getSourceCode for function calls

--- flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/model/node/FunctionCall.java
+++ flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/model/node/FunctionCall.java
@@ -1,10 +1,14 @@
 package io.ballerina.flowmodelgenerator.core.model.node;
 
+import io.ballerina.compiler.syntax.tree.SyntaxKind;
 import io.ballerina.flowmodelgenerator.core.central.Central;
 import io.ballerina.flowmodelgenerator.core.central.CentralProxy;
 import io.ballerina.flowmodelgenerator.core.model.Codedata;
 import io.ballerina.flowmodelgenerator.core.model.FlowNode;
 import io.ballerina.flowmodelgenerator.core.model.NodeBuilder;
+import io.ballerina.flowmodelgenerator.core.model.SourceBuilder;
+
+import java.util.Set;
 
 public class FunctionCall extends NodeBuilder {
 
@@ -22,6 +26,18 @@ public class FunctionCall extends NodeBuilder {
 
     @Override
     public String toSource(FlowNode flowNode) {
-        return null;
+        SourceBuilder sourceBuilder = new SourceBuilder(flowNode)
+                .newVariable();
+
+        if (flowNode.hasFlag(FlowNode.NODE_FLAG_CHECKED)) {
+            sourceBuilder.token().keyword(SyntaxKind.CHECK_KEYWORD);
+        }
+
+        FlowNode nodeTemplate = central.getNodeTemplate(flowNode.codedata());
+        return sourceBuilder.token()
+                .name(nodeTemplate.metadata().label())
+                .stepOut()
+                .functionParameters(nodeTemplate, Set.of("variable", "type"))
+                .build(false);
     }
 }
