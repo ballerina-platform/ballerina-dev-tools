a134e559bd54e909d340db3990f0a01a89791a17
Nipuna Fernando <nipunafe@gmail.com>
2024-08-14 15:54:00 +0530

    Update the existing getSourceCode test cases

--- flow-model-generator/modules/flow-model-generator-ls-extension/src/test/resources/to_source/config/generate.py
+++ flow-model-generator/modules/flow-model-generator-ls-extension/src/test/resources/to_source/config/generate.py
@@ -19,11 +19,19 @@ under the License.
 import os
 import json
 import sys
+from typing import OrderedDict
+
+
+def remove_duplicates(output_nodes):
+    # Use OrderedDict to preserve order and remove duplicates
+    unique_output_nodes = list(OrderedDict((json.dumps(node, sort_keys=True), node) for node in output_nodes).values())
+    return unique_output_nodes
 
 
 def write_json(output_node, output_filename):
     # Create the JSON structure
     output_data = {
+        "source": f"{output_filename.split('.json')[0]}/main.bal",
         "description": "Sample diagram node",
         "output": [],
         "diagram": output_node
@@ -47,7 +55,7 @@ def process_file_code(file_code):
         diagram_dir_name) if file.startswith(file_code)])
 
     # Iterate over the matching files
-    index = 1
+    output_nodes = []
     for file_name in matching_files:
         # Construct the file path
         file_path = os.path.join(diagram_dir_name, file_name)
@@ -55,30 +63,48 @@ def process_file_code(file_code):
         # Read the JSON file
         with open(file_path, 'r') as json_file:
             data = json.load(json_file)
+            codedata_type = input_node_name.upper()
 
             # Get the nodes array from the diagram property
             nodes = data['diagram']['nodes']
 
             # Find the nodes with the specified kind
             for node in nodes:
-                if node['codedata']['node'] == input_node_name.upper():
-                    write_json(node, f"{file_code}{index}.json")
-                    index += 1
+                if node['codedata']['node'] == codedata_type:
+                    output_nodes.append(node)
+
+            if codedata_type == "NEW_CONNECTION":
+                connections = data['diagram']['connections']
+                for connection in connections:
+                    if connection['codedata']['node'] == codedata_type:
+                        output_nodes.append(connection)
+
+    # Write each node to a JSON file
+    output_nodes_set = remove_duplicates(output_nodes)
+    for index, node in enumerate(output_nodes_set):
+        write_json(node, f"{file_code}{index+1}.json")
 
     # Add a test case for the template file
-    template_file = os.path.join(
-        current_path, "..", "node_template", f"{file_code}.json")
+    template_file_dir = os.path.join(current_path, "..", "..", "node_template")
+    matching_template_files = sorted([file for file in os.listdir(
+        template_file_dir) if file.startswith(file_code)])
 
-    # Read the JSON template file
-    with open(template_file, 'r') as template_file:
-        template_data = json.load(template_file)
-        output_property = template_data['output']
-        output_property['codedata']['lineRange'] = {
-            "fileName": "test.bal",
-            "startLine": {"line": 0, "offset": 0},
-            "endLine": {"line": 0, "offset": 0}
-        }
-        write_json(output_property, f"{file_code}0.json")
+    # Iterate over the matching template files
+    for matching_template_file in matching_template_files:
+        # Construct the file path
+        template_file_name = os.path.join(
+            template_file_dir, matching_template_file)
+
+        # Read the JSON template file
+        with open(template_file_name, 'r') as template_file:
+            template_data = json.load(template_file)
+            output_property = template_data['output']
+            output_property['codedata']['lineRange'] = {
+                "fileName": "test.bal",
+                "startLine": {"line": 0, "offset": 0},
+                "endLine": {"line": 0, "offset": 0}
+            }
+            write_json(output_property, matching_template_file)
 
 
 # Get the user input from the command line argument
