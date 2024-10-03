10c818596b6b3a41773d69981082ae33e5c2ef6e
Nipuna Fernando <nipunafe@gmail.com>
2024-10-03 23:40:52 +0530

    Mark the entire if node as suggested

--- flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/SuggestedModelGenerator.java
+++ flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/SuggestedModelGenerator.java
@@ -56,6 +56,7 @@ public class SuggestedModelGenerator {
                 } else {
                     i = handleSuggestedNode(newNodes, i, newNode);
                 }
+                setSuggestedNodes(newNode);
                 continue;
             }
 
