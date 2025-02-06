2abffdc8a8093e118a0514b6c005412f710a7c14
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2025-02-06 23:54:46 +0530

    Add issue with http param type

--- service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/ServiceModelGeneratorConstants.java
+++ service-model-generator/modules/service-model-generator-ls-extension/src/main/java/io/ballerina/servicemodelgenerator/extension/ServiceModelGeneratorConstants.java
@@ -53,6 +53,8 @@ public class ServiceModelGeneratorConstants {
     public static final String KIND_RESOURCE = "RESOURCE";
     public static final String KIND_REMOTE = "REMOTE";
     public static final String KIND_DEFAULT = "DEFAULT";
+    public static final String KIND_REQUIRED = "REQUIRED";
+    public static final String KIND_DEFAULTABLE = "DEFAULTABLE";
 
     public static final String PARAMETER = "parameter";
     public static final String SERVICE = "service";
