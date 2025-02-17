f904c4e9b082825f98878c2de67da0a1c0d0bc91
dulaj <dulaj@wso2.com>
2025-02-17 09:55:53 +0530

    Remove unnecessary import

--- flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/utils/TypeTransformer.java
+++ flow-model-generator/modules/flow-model-generator-core/src/main/java/io/ballerina/flowmodelgenerator/core/utils/TypeTransformer.java
@@ -61,7 +61,6 @@ import io.ballerina.projects.Document;
 import io.ballerina.projects.Module;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
