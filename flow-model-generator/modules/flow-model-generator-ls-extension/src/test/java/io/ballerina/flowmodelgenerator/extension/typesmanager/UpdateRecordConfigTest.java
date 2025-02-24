97d65dd0f54d8ca3dfb2ea8884ea1555876fa6be
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2025-02-24 22:16:33 +0530

    Impl updateRecordConfig api

---
+++ flow-model-generator/modules/flow-model-generator-ls-extension/src/test/java/io/ballerina/flowmodelgenerator/extension/typesmanager/UpdateRecordConfigTest.java
@@ -0,0 +1,83 @@
+/*
+ *  Copyright (c) 2025, WSO2 LLC. (http://www.wso2.com)
+ *
+ *  WSO2 LLC. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
+package io.ballerina.flowmodelgenerator.extension.typesmanager;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import io.ballerina.flowmodelgenerator.core.model.Codedata;
+import io.ballerina.flowmodelgenerator.extension.request.UpdatedRecordConfigRequest;
+import io.ballerina.modelgenerator.commons.AbstractLSTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Test cases for retrieving the record config model.
+ *
+ * @since 2.0.0
+ */
+public class UpdateRecordConfigTest extends AbstractLSTest {
+
+    @Override
+    @Test(dataProvider = "data-provider")
+    public void test(Path config) throws IOException {
+        Path configJsonPath = configDir.resolve(config);
+        TestConfig testConfig = gson.fromJson(Files.newBufferedReader(configJsonPath), TestConfig.class);
+        UpdatedRecordConfigRequest request = new UpdatedRecordConfigRequest(
+                getSourcePath(testConfig.filePath()),
+                testConfig.codedata(),
+                testConfig.typeConstraint(), testConfig.expr());
+        JsonObject response = getResponse(request);
+        JsonElement configResponse = response.get("recordConfig");
+        if (!configResponse.equals(testConfig.output())) {
+            TestConfig updateConfig = new TestConfig(testConfig.filePath(), testConfig.description(),
+                    testConfig.codedata(), testConfig.typeConstraint(), testConfig.expr(), configResponse);
+             updateConfig(configJsonPath, updateConfig);
+            compareJsonElements(configResponse, testConfig.output());
+            Assert.fail(String.format("Failed test: '%s' (%s)", testConfig.description(), configJsonPath));
+        }
+    }
+
+    @Override
+    protected String getResourceDir() {
+        return "update_record_config";
+    }
+
+    @Override
+    protected Class<? extends AbstractLSTest> clazz() {
+        return UpdateRecordConfigTest.class;
+    }
+
+    @Override
+    protected String getApiName() {
+        return "updateRecordConfig";
+    }
+
+    @Override
+    protected String getServiceName() {
+        return "typesManager";
+    }
+
+    private record TestConfig(String filePath, String description, Codedata codedata,
+                              String typeConstraint, String expr, JsonElement output) {
+    }
+}
