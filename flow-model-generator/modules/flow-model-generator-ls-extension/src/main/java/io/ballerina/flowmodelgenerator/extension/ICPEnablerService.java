f1af92ef433fe0adc07f5f09a1291721c9b7bd53
LakshanWeerasinghe <lak98weerasinghe@gmail.com>
2025-02-26 18:26:52 +0530

    Impl addIcp apis

--- flow-model-generator/modules/flow-model-generator-ls-extension/src/main/java/io/ballerina/flowmodelgenerator/extension/ICPEnablerService.java
+++ flow-model-generator/modules/flow-model-generator-ls-extension/src/main/java/io/ballerina/flowmodelgenerator/extension/ICPEnablerService.java
@@ -39,14 +39,21 @@ import io.ballerina.toml.semantic.ast.TomlKeyValueNode;
 import io.ballerina.toml.semantic.ast.TomlTableNode;
 import io.ballerina.toml.semantic.ast.TopLevelNode;
 import org.ballerinalang.annotation.JavaSPIService;
+import org.ballerinalang.langserver.common.utils.CommonUtil;
+import org.ballerinalang.langserver.common.utils.PositionUtil;
 import org.ballerinalang.langserver.commons.service.spi.ExtendedLanguageServerService;
 import org.ballerinalang.langserver.commons.workspace.WorkspaceManager;
+import org.eclipse.lsp4j.TextEdit;
 import org.eclipse.lsp4j.jsonrpc.services.JsonRequest;
 import org.eclipse.lsp4j.jsonrpc.services.JsonSegment;
 import org.eclipse.lsp4j.services.LanguageServer;
 
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
@@ -62,6 +69,9 @@ public class ICPEnablerService implements ExtendedLanguageServerService {
 
     private static final String BALLERINAX = "ballerinax";
     private static final String MODULE_NAME = "wso2.controlplane";
+    private static final String IMPORT_STMT = "import ballerinax/wso2.controlplane as _;%n";
+    private static final String REMOTE_MANAGEMENT_TRUE = "remoteManagement = true%n";
+    private static final String BUILD_OPTIONS = "%n[build-options]%n";
 
     private WorkspaceManager workspaceManager;
 
@@ -128,6 +138,67 @@ public class ICPEnablerService implements ExtendedLanguageServerService {
         return CompletableFuture.supplyAsync(() -> {
             CommonSourceResponse response = new CommonSourceResponse();
             try {
+                Path filePath = Path.of(request.projectPath());
+                Project project = this.workspaceManager.loadProject(filePath);
+                Package pkg = project.currentPackage();
+                Module defaultModule = pkg.getDefaultModule();
+                Collection<DocumentId> documentIds = defaultModule.documentIds();
+                boolean hasCorrectImport = false;
+                for (DocumentId documentId : documentIds) {
+                    if (hasCorrectImport) {
+                        break;
+                    }
+                    Document document = defaultModule.document(documentId);
+                    ModulePartNode root = document.syntaxTree().rootNode();
+                    NodeList<ImportDeclarationNode> imports = root.imports();
+                    for (ImportDeclarationNode importNode : imports) {
+                        if (validOrg(importNode) && validModuleName(importNode) && validPrefix(importNode)) {
+                            hasCorrectImport = true;
+                            break;
+                        }
+                    }
+                }
+                Map<String, List<TextEdit>> textEdits = new HashMap<>();
+                response.setTextEdits(textEdits);
+                if (!hasCorrectImport) {
+                    Path mainPath = project.sourceRoot().resolve("main.bal");
+                    Optional<Document> document = workspaceManager.document(mainPath);
+                    if (document.isEmpty()) {
+                        throw new RuntimeException("main.bal not found");
+                    }
+                    Node node = document.get().syntaxTree().rootNode();
+                    TextEdit edit = new TextEdit(PositionUtil.toRange(node.lineRange().startLine()), IMPORT_STMT);
+                    textEdits.put(mainPath.toString(), List.of(edit));
+                }
+                Optional<BallerinaToml> ballerinaToml = pkg.ballerinaToml();
+                if (ballerinaToml.isEmpty()) {
+                    throw new RuntimeException("Ballerina.toml not found");
+                }
+                Path tomlPath = project.sourceRoot().resolve("Ballerina.toml");
+                TomlTableNode tomlTableNode = ballerinaToml.get().tomlAstNode();
+                TopLevelNode topLevelNode = tomlTableNode.entries().get("build-options");
+                if (topLevelNode instanceof TomlTableNode buildOptions) {
+                    TopLevelNode icpNode = buildOptions.entries().get("remoteManagement");
+                    if (icpNode instanceof TomlKeyValueNode keyValueNode) {
+                        String value = keyValueNode.value().toNativeValue().toString();
+                        if (value.trim().equals("true")) {
+                            return response;
+                        } else {
+                            TextEdit edit = new TextEdit(
+                                    PositionUtil.toRange(icpNode.location().lineRange()), REMOTE_MANAGEMENT_TRUE);
+                            textEdits.put(tomlPath.toString(), List.of(edit));
+                        }
+                    } else {
+                        TextEdit edit = new TextEdit(
+                                PositionUtil.toRange(buildOptions.location().lineRange().endLine()), REMOTE_MANAGEMENT_TRUE);
+                        textEdits.put(tomlPath.toString(), List.of(edit));
+                    }
+                } else {
+                    TextEdit edit = new TextEdit(
+                            PositionUtil.toRange(tomlTableNode.location().lineRange().endLine()),
+                            BUILD_OPTIONS + REMOTE_MANAGEMENT_TRUE);
+                    textEdits.put(tomlPath.toString(), List.of(edit));
+                }
             } catch (Throwable e) {
                 response.setError(e);
             }
